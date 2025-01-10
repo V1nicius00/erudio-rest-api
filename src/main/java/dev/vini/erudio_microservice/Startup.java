0420235b4d462917b7359da75d0b83fd75d12bde
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-09 23:13:59 -0300

    refactor: changing main file name

--- src/main/java/dev/vini/erudio_microservice/Startup.java
+++ src/main/java/dev/vini/erudio_microservice/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ErudioMicroserviceApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ErudioMicroserviceApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
