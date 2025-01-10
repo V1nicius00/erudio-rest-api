e48871e5ef593a876e9c10fd1836c1e5aac8c132
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-09 23:02:42 -0300

    creating project

---
+++ src/test/java/dev/vini/erudio_microservice/ErudioMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vini.erudio_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErudioMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
