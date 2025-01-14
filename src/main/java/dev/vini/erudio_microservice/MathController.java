eb99f98d1ddca4f7a557122158c2a51609f4a2ab
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-13 22:51:56 -0300

    feat: creating sum endpoint

--- src/main/java/dev/vini/erudio_microservice/MathController.java
+++ src/main/java/dev/vini/erudio_microservice/MathController.java
@@ -15,7 +15,26 @@ public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
-            @PathVariable(value = "numberTwo") String numberTwo){
-        return 1D;
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception{
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new Exception();
+        }
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        String number = strNumber.replace(",",".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+        String number = strNumber.replace(",",".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
