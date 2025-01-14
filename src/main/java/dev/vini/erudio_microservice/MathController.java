e1724e8a42803662bbd2cec4b11ca74284492d04
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-13 22:38:44 -0300

    feat: changing GreetingController to MathController

---
+++ src/main/java/dev/vini/erudio_microservice/MathController.java
@@ -0,0 +1,21 @@
+package dev.vini.erudio_microservice;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo){
+        return 1D;
+    }
+}
