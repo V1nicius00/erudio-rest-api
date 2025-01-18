98b40fcdcb5e4729bc474692a12cd943671f0a54
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-18 18:23:14 -0300

    feat: adding MathService functions on MathController

--- src/main/java/dev/vini/erudio_microservice/MathController.java
+++ src/main/java/dev/vini/erudio_microservice/MathController.java
@@ -1,6 +1,7 @@
 package dev.vini.erudio_microservice;
 
 import dev.vini.erudio_microservice.exception.UnsupportedMathOperationException;
+import dev.vini.erudio_microservice.service.MathService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,15 +14,21 @@ public class MathController {
 
     private final AtomicLong counter = new AtomicLong();
 
+    private final MathService mathService;
+
+    public MathController(MathService mathService) {
+        this.mathService = mathService;
+    }
+
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception{
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+        if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)){
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        return mathService.sum(numberOne, numberTwo);
     }
 
     @RequestMapping("/sub/{numberOne}/{numberTwo}")
@@ -29,10 +36,10 @@ public class MathController {
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception{
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+        if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)){
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return mathService.subtraction(numberOne, numberTwo);
     }
 
     @RequestMapping("/mult/{numberOne}/{numberTwo}")
@@ -40,10 +47,10 @@ public class MathController {
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception{
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+        if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)){
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+        return mathService.multiplication(numberOne, numberTwo);
     }
 
     @RequestMapping("/div/{numberOne}/{numberTwo}")
@@ -51,10 +58,10 @@ public class MathController {
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception{
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+        if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)){
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return mathService.division(numberOne, numberTwo);
     }
 
     @RequestMapping("/mean/{numberOne}/{numberTwo}")
@@ -62,32 +69,21 @@ public class MathController {
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception{
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+        if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)){
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+        return mathService.mean(numberOne, numberTwo);
     }
 
     @RequestMapping("/squareroot/{number}")
     public Double squareRoot(
             @PathVariable(value = "number") String number
     ) throws Exception{
-        if(!isNumeric(number)){
+        if(!mathService.isNumeric(number)){
             throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
-        return Math.sqrt(convertToDouble(number));
+        return mathService.squareRoot(number);
     }
 
-    private Double convertToDouble(String strNumber) {
-        if(strNumber == null) return 0D;
-        String number = strNumber.replace(",",".");
-        if(isNumeric(number)) return Double.parseDouble(number);
-        return 0D;
-    }
 
-    private boolean isNumeric(String strNumber) {
-        if(strNumber == null) return false;
-        String number = strNumber.replace(",",".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
 }
