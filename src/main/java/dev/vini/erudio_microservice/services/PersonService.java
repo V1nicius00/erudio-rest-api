6115c2fdab378b91a18d4df0c920e4042614b93f
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-21 23:14:15 -0300

    feat: creating findAllPeople service

--- src/main/java/dev/vini/erudio_microservice/services/PersonService.java
+++ src/main/java/dev/vini/erudio_microservice/services/PersonService.java
@@ -4,6 +4,8 @@ import dev.vini.erudio_microservice.model.Person;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -27,4 +29,22 @@ public class PersonService {
         return person;
     }
 
+    public List<Person> findAllPeople() {
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i <= 8; i++){
+            Person person = mockPeople(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPeople(int i){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Brazil " + 1);
+        person.setGender("Male " + 1);
+        return person;
+    }
 }
