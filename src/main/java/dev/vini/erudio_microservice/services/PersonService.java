f1d76cad19b6813c88f03ed718b659e2456e197c
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-21 22:54:14 -0300

    feaet: adding Person attributes

--- src/main/java/dev/vini/erudio_microservice/services/PersonService.java
+++ src/main/java/dev/vini/erudio_microservice/services/PersonService.java
@@ -1,4 +1,57 @@
 package dev.vini.erudio_microservice.services;
 
-public class PersonService {
+import java.io.Serializable;
+
+public class PersonService implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public PersonService() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
