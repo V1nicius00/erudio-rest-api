21a760f33458c899d34a45c559e1b1be4536f36d
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-21 23:06:32 -0300

    feat: creating find person by id service

--- src/main/java/dev/vini/erudio_microservice/services/PersonService.java
+++ src/main/java/dev/vini/erudio_microservice/services/PersonService.java
@@ -1,8 +1,30 @@
 package dev.vini.erudio_microservice.services;
 
-import java.io.Serializable;
+import dev.vini.erudio_microservice.model.Person;
+import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
 public class PersonService {
 
+    private final AtomicLong counter = new AtomicLong();
+
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public Person findbyId(String id){
+
+        logger.info("Finding one person...");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name");
+        person.setLastName("Last Name");
+        person.setAddress("Brazil");
+        person.setGender("Male");
+        return person;
+    }
 
 }
