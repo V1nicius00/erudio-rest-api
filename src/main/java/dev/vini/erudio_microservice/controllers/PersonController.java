27d3bf1d4cfba610187bdc051e5fa1a140f8815d
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-21 23:14:45 -0300

    feat: creating findAllPeople endpoint

--- src/main/java/dev/vini/erudio_microservice/controllers/PersonController.java
+++ src/main/java/dev/vini/erudio_microservice/controllers/PersonController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -19,9 +21,15 @@ public class PersonController {
     }
 
     @RequestMapping(value = "/{id}",
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE)
+                    method = RequestMethod.GET,
+                    produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id){
         return personService.findbyId(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET,
+                    produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAllPeople(){
+        return personService.findAllPeople();
     }
 }
