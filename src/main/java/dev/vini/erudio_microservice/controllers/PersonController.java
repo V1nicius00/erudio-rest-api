04da57a69cf5f00db839064e6aede78a5c053f14
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-21 23:06:01 -0300

    feat: creating find person by id endpoint

--- src/main/java/dev/vini/erudio_microservice/controllers/PersonController.java
+++ src/main/java/dev/vini/erudio_microservice/controllers/PersonController.java
@@ -1,4 +1,27 @@
 package dev.vini.erudio_microservice.controllers;
 
+import dev.vini.erudio_microservice.model.Person;
+import dev.vini.erudio_microservice.services.PersonService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
 public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @RequestMapping(value = "/{id}",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id){
+        return personService.findbyId(id);
+    }
 }
