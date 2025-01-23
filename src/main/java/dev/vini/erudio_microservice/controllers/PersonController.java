bce0a8a5a14ed41aa6a20daaea8a1ccb6fdac6cc
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-01-22 22:45:13 -0300

    feat: adding create person endpoint

--- src/main/java/dev/vini/erudio_microservice/controllers/PersonController.java
+++ src/main/java/dev/vini/erudio_microservice/controllers/PersonController.java
@@ -3,10 +3,7 @@ package dev.vini.erudio_microservice.controllers;
 import dev.vini.erudio_microservice.model.Person;
 import dev.vini.erudio_microservice.services.PersonService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,15 +18,22 @@ public class PersonController {
     }
 
     @RequestMapping(value = "/{id}",
-                    method = RequestMethod.GET,
-                    produces = MediaType.APPLICATION_JSON_VALUE)
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id){
         return personService.findbyId(id);
     }
 
     @RequestMapping(method = RequestMethod.GET,
-                    produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAllPeople(){
         return personService.findAllPeople();
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person){
+        return personService.create(person);
     }
 }
