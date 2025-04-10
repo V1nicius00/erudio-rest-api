3cfc315e4846f0a9897f19bb4f9d2ee87b3b5403
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-04-09 23:14:03 -0300

    feat: adding findByGender on repository

--- src/main/java/dev/vini/erudio_microservice/repositories/PersonRepository.java
+++ src/main/java/dev/vini/erudio_microservice/repositories/PersonRepository.java
@@ -4,5 +4,9 @@ import dev.vini.erudio_microservice.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {}
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByGender();
+}
