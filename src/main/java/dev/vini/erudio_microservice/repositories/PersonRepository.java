3091af01fc282965c5c87024ad379c330e331e58
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2025-04-09 23:16:43 -0300

    feat: adding findByGender parameter

--- src/main/java/dev/vini/erudio_microservice/repositories/PersonRepository.java
+++ src/main/java/dev/vini/erudio_microservice/repositories/PersonRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    List<Person> findByGender();
+    List<Person> findByGender(String gender);
 }
