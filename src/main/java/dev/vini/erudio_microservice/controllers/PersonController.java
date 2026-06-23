5659abca5f5f7ecce415023ffea660f5e52b0d25
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2026-06-22 23:19:27 -0300

    feat: create documentation to find all people endpoint

--- src/main/java/dev/vini/erudio_microservice/controllers/PersonController.java
+++ src/main/java/dev/vini/erudio_microservice/controllers/PersonController.java
@@ -3,6 +3,11 @@ package dev.vini.erudio_microservice.controllers;
 import dev.vini.erudio_microservice.data.dto.PersonDTO;
 import dev.vini.erudio_microservice.models.Person;
 import dev.vini.erudio_microservice.services.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +28,25 @@ public class PersonController {
 
     @GetMapping(value = "/id/{id}",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Operation(summary = "Find all People",
+        description = "Finds all People",
+        tags = {"People"},
+        responses = {
+            @ApiResponse(description = "Success",
+                    responseCode = "200",
+                    content = {
+                        @Content(
+                                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class))
+                        )
+                    }),
+            @ApiResponse(description = "No content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+        }
+    )
     public PersonDTO findById(@PathVariable(value = "id") Long id){
         return personService.findById(id);
     }
